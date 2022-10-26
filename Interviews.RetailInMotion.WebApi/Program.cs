00ef13e24158524faf880aef7a05f21230524c41
Eduardo Julião <eduardo.juliaojr@outlook.com>
2022-10-26 19:00:00 +0100

    References

--- Interviews.RetailInMotion.WebApi/Program.cs
+++ Interviews.RetailInMotion.WebApi/Program.cs
@@ -1,11 +1,35 @@
+using Interviews.RetailInMotion.Domain.Interfaces.Repositories;
+using Interviews.RetailInMotion.Domain.Interfaces.Services;
+using Interviews.RetailInMotion.Domain.Services;
+using Interviews.RetailInMotion.Repository;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var services = builder.Services;
 
-builder.Services.AddControllers();
+services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
+
+services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseInMemoryDatabase("InterviewDb")
+);
+
+services.AddSingleton<IOrderService, OrderService>();
+services.AddSingleton<IStockService, StockService>();
+services.AddSingleton<IOrderRepository, OrderRepository>();
+services.AddSingleton<IStockRepository, StockRepository>();
+
+var serilogger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .Enrich.FromLogContext()
+    .CreateLogger();
+
+builder.Logging.AddSerilog(serilogger);
 
 var app = builder.Build();
 
