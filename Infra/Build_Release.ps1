a6e2f81c8c05361030f25b9881a6c926da55bb90
Eduardo Julião <eduardo.juliaojr@outlook.com>
2022-10-31 17:55:57 +0000

    release script

---
+++ Infra/Build_Release.ps1
@@ -0,0 +1,7 @@
+$apiPath = "$pwd/Interviews.RetailInMotion.WebApi/Interviews.RetailInMotion.WebApi.csproj"
+$releaseFolder = "$pwd/Interviews.RetailInMotion.WebApi/release"
+
+dotnet build  $apiPath
+dotnet publish $apiPath --output $releaseFolder --sc true --runtime "win-x64"
+
+Start-Process "$releaseFolder/Interviews.RetailInMotion.WebApi.exe"
